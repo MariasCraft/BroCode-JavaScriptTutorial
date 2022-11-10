c214fc2b7f8a09d5e14d0ad0ee954abcc106c631
Maria <codestantly@gmail.com>
2022-11-10 17:04:34 +0800

    get the RIGHT ANGLE TRIANGLE - HYPOTENUSE formula in JS code

---
+++ index.js
@@ -0,0 +1,122 @@
+// --- THIS IS THE BEGINNING OF JAVASCRIPT
+// console.log ('I like rex');
+// console.log ('I will be a Software Engineer');
+
+// window.alert ("I WILL BE A SOFTWARE ENGINEER")
+
+
+
+// --- VARIABLES
+          // Variable is a container for storing data
+          // Variable behaves as of it was the value that it contains
+
+          // Two steps :
+          // 1. Declaration (var,let,const)
+          // 2. Assignment (= assignment operator)
+
+          // Data types : string, number, boolean
+
+          /*
+          let age = 21;
+          let firstName = 'Maria';
+          let student = true;
+          // age = 21;
+
+          console.log (age);
+          console.log (firstName);
+          console.log (student);
+
+          document.getElementById("p1").innerHTML = "Hello " + firstName;
+          document.getElementById("p2").innerHTML = "You are " + age + " years old.";
+          document.getElementById("p3").innerHTML = "Enrolled: " + student;
+          */
+
+                              // Arithmetic expression is a combination of. . . 
+                              /* operands (values, variables, etc.)
+                                  operators (+ - * / %)
+                                  that can be evaluated to a value
+                                  example: y=x+5 (like algebra) */
+
+           /*  let student = 20;
+
+          // student = student +1;
+          // student = student - 1;
+          // student = student * 2;
+          // student = student / 2;
+          // (modulus % -- will show the remainder what was divided by)
+           /* let extraStudents = student % 3;  //---> 20/3 = 6.667 (3*6=18), so 20-18=2 (2 is the answer of the modulus) */
+
+           /* other ways of writing code above : 
+                    students += 1;
+                    studetns -= 1;
+                    students *= 2;
+                    students /= 2; */
+
+          /* console.log (extraStudents) */
+
+          //---------------------------------------------------------------------------------
+          // --- USER INPUT
+                              // how to accept user input, EASY WAY with window prompt
+
+          // let username = window.prompt("What is your name? ");
+
+          // console.log (username);
+
+                              // DIFFICULT WAY but practical
+
+       
+          
+          // username = document.getElementById("myButton").onclick = function(){
+
+          //           username = document.getElementById("myText").value;
+                    // console.log(username);
+                    // document.getElementById("myLabel").innerHTML;
+
+          //---------------------------------------------------------------------------------
+
+          //--- TYPE CONVERSION = change the datatype of a value to another (strings, numbers, booleans)
+
+          // let age = window.prompt ("How old are you?");
+
+          // // age += 1; // this is a string - to change it to a number, see syntax below
+
+          // age = Number(age);
+
+          // age += 1;
+
+          // console.log("Happy Birthday! You are", age, "years old");
+          
+          //-- another example : 
+
+          // let x;
+          // let y;
+          // let z;
+
+          // x= Number("3.14");
+          // y= String(3.14);
+          // z= Boolean(""); -- if boolean is empty it will give a false result, if you write something, you will get a true result
+
+          // console.log(x, typeof x)
+          // console.log(y, typeof y)
+          // console.log(z, typeof z)  
+          
+          //---------------------------------------------------------------------------------
+
+          // CONST - a variable that can't be changed
+            //example :
+
+            // const PI = 3.14159;   //let change to const since the pi in math cannot be changed just like const.
+            // let radius ;
+            // let circumference;
+
+            // radius = window.prompt ("Enter the radius of a circle");
+            // radius = Number(radius);
+
+            // circumference = 2 * PI * radius;
+
+            // console.log ("The circumference is: ", circumference)
+
+
+            // ---- MATH
+
+            
