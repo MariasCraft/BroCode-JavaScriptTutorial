7c076f708a0a66514ab516f50d89dc7479ef1514
Maria <codestantly@gmail.com>
2022-11-12 02:09:13 +0800

    creating HYPOTENUSE CAL

--- index.js
+++ index.js
@@ -144,16 +144,31 @@
 
     // --- FIND THE RIGHT ANGLE TRIANGLE - HYPOTENUSE
 
-        let a;
-        let b;
-        let c;
+        // let a;
+        // let b;
+        // let c;
 
-        a = window.prompt ("Enter side A")
-        a = Number(a);
+        // a = window.prompt ("Enter side A")
+        // a = Number(a);
 
-        b = window.prompt ("Enter side B")
-        b = Number(a);
+        // b = window.prompt ("Enter side B")
+        // b = Number(a);
 
-        c = Math.sqrt (Math.pow (a, 2) + Math.pow (b, 2));
+        // c = Math.sqrt (Math.pow (a, 2) + Math.pow (b, 2));
 
-        console.log ("Side C:", c);
+        // console.log ("Side C:", c);
+
+        // HYPOTENUSE IN HTML
+
+        // document.getElementById ("submitButton").onclick = function () {
+
+        //     a = document.getElementById("aText").value; 
+        //     a = Number(a);
+
+        //     b = document.getElementById("bText").value; 
+
+        //     c = Math.sqrt (Math.pow (a, 2) + Math.pow (b, 2));
+
+        //    document.getElementById("cLabel").innerHTML = "side c: " + c;
+
+        // }
